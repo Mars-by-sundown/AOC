bdeb7f12e29946d54ceba371f469899fd962bcf7
Nicholas Ragano <nragano@gmail.com>
2025-12-07 21:14:28 -0600

    Part one completed

--- 2025/Day_3.py
+++ 2025/Day_3.py
@@ -1,6 +1,6 @@
 def part_1(inputs):
     rolls = 0
-    print(inputs)
+    [print(x) for x in inputs]
     n = len(inputs[0])
     m = len(inputs)
     numAdj = [[0]*n for x in range(0,m)]
@@ -10,18 +10,19 @@ def part_1(inputs):
             [-1,1],[0,1],[1,1]]
     for row in range(0,m):
         for col in range(0,n):
-            for each in dirs:
-                x = col + each[0]
-                y = row + each[1]
-                if (x >= 0) and (x < n) and (y >= 0) and (y < m):
-                    if inputs[x][y] == '@':
-                        # print(row,col,numAdj[row][col])
-                        numAdj[col][row] += 1
-                        rollset.add((row,col))
+            if inputs[row][col] == "@":
+                for each in dirs:
+                    x = col + each[0]
+                    y = row + each[1]
+                    if (x >= 0) and (x < n) and (y >= 0) and (y < m):
+                        if inputs[y][x] == '@':
+                            # print(row,col,numAdj[row][col])
+                            numAdj[row][col] += 1
+                rollset.add((row,col))
     for each in numAdj:
         print(each)
     for each in rollset:
-        if numAdj[each[0]][each[1]] < 4 and inputs[each[0]][each[1]] == '@':
+        if numAdj[each[0]][each[1]] < 4:
             rolls += 1
 
 
@@ -29,6 +30,6 @@ def part_1(inputs):
 
 
 
-fl = open("input.txt","r")
+fl = open("testcase.txt","r")
 inputs = [str(x).strip() for x in fl.readlines()]
 print(part_1(inputs))
